dd66642fde72f5682c91d54cccd6f0115ac19358
Dan <dan@dantacular.(none)>
2014-05-31 04:58:56 -0700

    Changed psql to psql81

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATA="$( cd "$DIR"/../../data && pwd)"
-psql81 -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
-psql81 -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
-cat $DIR/../src/load_data.sql | eval "sed -e 's|<!DATA>|$DATA|g'" | psql81 -p $PGPORT $DB_NAME
+psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
+cat $DIR/../src/load_data.sql | eval "sed -e 's|<!DATA>|$DATA|g'" | psql -p $PGPORT $DB_NAME
 
