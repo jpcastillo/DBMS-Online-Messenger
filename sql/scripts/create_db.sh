8b4c3581ae8a7b73a4474a4941a0b2d81356f20b
Dan <dan@dantacular.(none)>
2014-05-31 03:19:50 -0700

    Relative directories for loading files

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATA="$( cd ../../data && pwd)"
 psql81 -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
 psql81 -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
-psql81 -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
+cat $DIR/../src/load_data.sql | eval "sed -e 's|<!DATA>|$DATA|g'" | psql81 -p $PGPORT $DB_NAME
+
--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,23 +1,23 @@
 COPY USER_LIST
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/usr_list.csv'
+FROM '<!DATA>/usr_list.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE user_list_list_id_seq RESTART 55906;
 
 COPY USR
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/usr.csv'
+FROM '<!DATA>/usr.csv'
 WITH DELIMITER ';';
 
 COPY USER_LIST_CONTAINS
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/usr_list_contains.csv'
+FROM '<!DATA>/usr_list_contains.csv'
 WITH DELIMITER ';';
 
 COPY CHAT
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/chat.csv'
+FROM '<!DATA>/chat.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE chat_chat_id_seq RESTART 5001;
 
 COPY CHAT_LIST
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/chat_list.csv'
+FROM '<!DATA>/chat_list.csv'
 WITH DELIMITER ';';
 
 COPY MESSAGE
@@ -26,15 +26,15 @@ COPY MESSAGE
 	msg_timestamp, 
 	sender_login,
 	chat_id)
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/message.csv'
+FROM '<!DATA>/message.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE message_msg_id_seq RESTART 50000;
 
 COPY MEDIA_ATTACHMENT
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/media_attachment.csv'
+FROM '<!DATA>/media_attachment.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE media_attachment_media_id_seq RESTART 2000;
 
 COPY NOTIFICATION
-FROM '/Users/Torcherist/Documents/school/Spring2014/cs166/project/03/code/data/notification.csv'
+FROM '<!DATA>/notification.csv'
 WITH DELIMITER ';';
