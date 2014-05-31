c90c6698badf5226503eb01ee6f8f78a936f2b13
Dan <dan@dantacular.(none)>
2014-05-31 04:56:00 -0700

    Bug Fix for create_db.sh

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DATA="$( cd ../../data && pwd)"
+DATA="$( cd "$DIR"/../../data && pwd)"
 psql81 -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
 psql81 -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
 cat $DIR/../src/load_data.sql | eval "sed -e 's|<!DATA>|$DATA|g'" | psql81 -p $PGPORT $DB_NAME
